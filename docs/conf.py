113b031df2f89d428ac8f16deb635cb1e2a5e2f1
szkuds <szkudx@gmail.com>
2026-04-15 18:29:57 +0200

    Figuring out the documantation generation

--- docs/conf.py
+++ docs/conf.py
@@ -61,7 +61,7 @@ templates_path = ["_templates"]
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "autoapi"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
